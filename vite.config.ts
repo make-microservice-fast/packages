d58da2c16bdbe705bcc0514101467ebae7b7b117
jackson.wen <jackson.wen@xm.wonder.com>
2025-10-24 17:45:08 +0800

    fix: update build:packages script to include CSS copy and clean option

--- vite.config.ts
+++ vite.config.ts
@@ -4,30 +4,30 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      // 确保外部化处理那些你不想打包进库的依赖
-      external: [
-        "react",
-        "react-dom",
-        "tailwindcss",
-        "class-variance-authority",
-        "clsx",
-        "tailwind-merge",
-        "lucide-react",
-      ],
-      output: {
-        // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          tailwindcss: "tailwindcss",
-          "class-variance-authority": "classVarianceAuthority",
-          clsx: "clsx",
-          "tailwind-merge": "tailwindMerge",
-          "lucide-react": "lucideReact",
-        },
-      },
-    },
-  },
+  // build: {
+  //   rollupOptions: {
+  //     // 确保外部化处理那些你不想打包进库的依赖
+  //     external: [
+  //       "react",
+  //       "react-dom",
+  //       "tailwindcss",
+  //       "class-variance-authority",
+  //       "clsx",
+  //       "tailwind-merge",
+  //       "lucide-react",
+  //     ],
+  //     output: {
+  //       // 在 UMD 构建模式下为这些外部化的依赖提供一个全局变量
+  //       globals: {
+  //         react: "React",
+  //         "react-dom": "ReactDOM",
+  //         tailwindcss: "tailwindcss",
+  //         "class-variance-authority": "classVarianceAuthority",
+  //         clsx: "clsx",
+  //         "tailwind-merge": "tailwindMerge",
+  //         "lucide-react": "lucideReact",
+  //       },
+  //     },
+  //   },
+  // },
 });
