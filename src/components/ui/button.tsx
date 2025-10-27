dba278aabfbb18dcd56a12bdd838076c3ec2de81
jackson.wen <jackson.wen@xm.wonder.com>
2025-10-27 16:36:22 +0800

    feat: test---1

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -2,10 +2,12 @@ import React, { type PropsWithChildren } from "react";
 
 export type ButtonProps = {
   className?: string;
-}; 
-
-const Button: React.FC<PropsWithChildren<ButtonProps>> = ({ ...props }) => {
-  return <button className="p-3 rounded border-solid border bg-red-500" {...props} />;
 };
 
-export default Button;
+const Button: React.FC<PropsWithChildren<ButtonProps>> = ({ ...props }) => {
+  return (
+    <button className="p-3 rounded border-solid border bg-red-500" {...props} />
+  );
+};
+
+export { Button };
