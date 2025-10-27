ded2f0a7252edf0eb510268416888ab43840265b
jackson.wen <jackson.wen@xm.wonder.com>
2025-10-27 16:13:12 +0800

    feat: release

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 import dts from 'vite-plugin-dts' // 用于生成 .d.ts 类型文件
 
-
+ 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),  dts({
